70efd31a65e72dccc21d9c7c3f9d8f148ff7c7f4
sbecenti <samsonbecenti@gmail.com>
2024-04-20 23:18:42 -0700

    implemented texture for cube

--- main.cpp
+++ main.cpp
@@ -145,6 +145,48 @@ int main() {
 
     // DEFINE TEXTURES HERE Project 10 --> NOTE FOR PROJECT 10
 
+    // Environment Map
+    GLuint skyboxTexture;
+    glGenTextures(1, &skyboxTexture);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTexture);
+
+    std::vector<std::string> faces = {
+        "negx.jpg",
+        "posx.jpg",
+        "negy.jpg",
+        "posy.jpg",
+        "negz.jpg",
+        "posz.jpg"
+    };
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char *data = SOIL_load_image(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 
+                        0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+            );
+            SOIL_free_image_data(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            SOIL_free_image_data(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    // Pass skybox texture to the cube shader
+    cubeShader.Use();
+    glUniform1i(glGetUniformLocation(cubeShader.Program, "skybox"), 0); // Set skybox texture unit
+
+
     glm::vec3 cylinderOffset(-0.4f, -2.98f, -0.15f); //Used to roughly overlap cylinder base with sphere base
 
     // Game Loop
@@ -169,6 +211,11 @@ int main() {
         glm::mat4 model = glm::mat4(1.0f); // Initialize model to be 4x4 identity
 
         // BIND TEXTURES HERE PROJECT 10
+
+        // Bind the skybox texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTexture);  
+
 
         // CHECKERBOARD
         checkerboardShader.Use(); // Use checkerboard shader
@@ -241,7 +288,7 @@ int main() {
 
         
         // CYLINDER
-        cylinderShader.Use(); // Activate cylinder shader
+        cylinderShader.Use();
 
         GLint cylinderColorLoc = glGetUniformLocation(cylinderShader.Program, "cylinderColor"); // Retrieve cylinderColor location
         lightColorLoc = glGetUniformLocation(cylinderShader.Program, "lightColor"); // Reset lightColor location
